6588327f834ebc97b55055f61a4890f954236a7c
Brian Wong <bwong92@gmail.com>
2019-08-13 04:30:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
   attr_accessor :name, :songs
-  @@song_count = 0
+  
   def initialize(name)
     @name = name
     @songs = []
@@ -13,7 +13,7 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-  
+
   end
 
   def add_song_by_name(song_name)
